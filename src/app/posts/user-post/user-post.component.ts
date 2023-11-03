751e2e7f698f2d7615be2101acc35d954352d80a
220083225 <220083225@tut4life.ac.za>
2023-11-03 13:10:29 +0200

    Posting

--- src/app/posts/user-post/user-post.component.ts
+++ src/app/posts/user-post/user-post.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 
 interface PostInfo{
@@ -17,21 +19,19 @@ interface PostInfo{
   styleUrls: ['./user-post.component.css']
 })
 export class UserPostComponent {
-  post_id = [1,2,3,4]
-  posts: { [id: string]: PostInfo } = { 
-    "post_info":{ user_name: "Sinenhlanhla Masuku",
-        user_postion: "Web Developer",
-        institution: "Tshwane University Of Technology",
-        user_image: "assets/Sneh.jpg",
-        post_time: "2hr",
-        text_message: "I'm thrilled to share that I've completed a graduate certificate course in project management with the president's honor roll.",
-        image_message: "assets/Sneh.jpg"},
-   "post_info2":{ user_name: "Thoko Masanabo",
-        user_postion: "Fullstack Developer",
-        institution: "Tshwane University Of Technology",
-        user_image: "assets/Sneh.jpg",
-        post_time: "2hr",
-        text_message: "I'm thrilled to share that I've completed a graduate certificate course in project management with the president's honor roll.",
-        image_message: "assets/Sneh.jpg"}
-         }
+
+  posts: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getPosts();
+  }
+
+  getPosts() {
+    this.http.get<any>('http://localhost:3000/posts').subscribe(response => {
+      this.posts = response.reverse();
+    });
+  }
+
 }
--- src/app/posts/your-thoughts/your-thoughts.component.ts
+++ src/app/posts/your-thoughts/your-thoughts.component.ts
@@ -1,44 +1,33 @@
 import { Component } from '@angular/core';
-
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+ 
 @Component({
   selector: 'app-your-thoughts',
   templateUrl: './your-thoughts.component.html',
   styleUrls: ['./your-thoughts.component.css']
 })
 export class YourThoughtsComponent {
-  // Functions for the functionalities
-  uploadPhoto(): void {
-    // Handle photo upload functionality
-    console.log('Upload photo');
-  }
+  
+  url='';
+  posts=[];
+  newPost:any ={name:'', imageUrl:'',postData:'',dateTime:''};
+  postData: string = '';
+  constructor(private http:HttpClient){}
 
-  uploadVideo(): void {
-    // Handle video upload functionality
-    console.log('Upload video');
-  }
+  
+getPosts() {
+  this.http.get<any>('http://localhost:3000/posts').subscribe(response => {
+    this.posts = response;
+  });
+}
 
-  createEvent(): void {
-    // Handle event creation functionality
-    console.log('Create event');
-  }
-
-  expressFeelingActivity(): void {
-    // Handle feeling/activity expression functionality
-    console.log('Express feeling/activity');
-  }
-
-  createPoll(): void {
-    // Handle create a poll functionality
-    console.log('Create a poll');
-  }
-
-  askQuestion(): void {
-    // Handle asking a question functionality
-    console.log('Ask a question');
-  }
-
-  help(): void {
-    // Handle help functionality
-    console.log('Help');
-  }
+onSubmit() {
+  const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  this.newPost.name ='Kabelo Axis';
+  this.newPost.dateTime = new Date();
+  this.http.post<any>('http://localhost:3000/posts', this.newPost, { headers }).subscribe(response => {
+    this.getPosts(); // Refresh the posts after a new post is created
+    this.newPost = { name:'', imageUrl:'',postData:'',timeDate: ''}; // Reset the new post object
+  });
+}
 }
